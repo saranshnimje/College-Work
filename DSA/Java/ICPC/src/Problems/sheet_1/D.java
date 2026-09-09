ab72ef0b4b9c6bea9fa3616b069f035368e47f1b
Saransh Nimje <saranshnimje0@gmail.com>
2026-09-09 19:51:06 +0530

    Create D.java

---
+++ DSA/Java/ICPC/src/Problems/sheet_1/D.java
@@ -0,0 +1,15 @@
+package Problems.sheet_1;
+
+import java.util.Scanner;
+public class D {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        long D = sc.nextLong();
+        long X = (A*B)-(C*D);
+        System.out.println("Difference = " + X);
+    }
+}
