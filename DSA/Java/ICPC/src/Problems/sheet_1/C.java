97b485559ce708a87cf960512d03b1699e1fc96f
Saransh Nimje <saranshnimje0@gmail.com>
2026-09-09 19:49:45 +0530

    Create C.java

---
+++ DSA/Java/ICPC/src/Problems/sheet_1/C.java
@@ -0,0 +1,14 @@
+package Problems.sheet_1;
+
+import java.util.Scanner;
+public class C {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long X = sc.nextLong();
+        long Y = sc.nextLong();
+        System.out.println(X + " + " + Y + " = " + (X+Y));
+        System.out.println(X + " * " + Y + " = " + (X*Y));
+        System.out.println(X + " - " + Y + " = " + (X-Y));
+    }
+}
