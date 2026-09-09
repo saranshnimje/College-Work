143c31e1d5261eddc19eb7a0d6ae86eddb31ef48
Saransh Nimje <saranshnimje0@gmail.com>
2026-09-09 19:45:28 +0530

    Create A.java

---
+++ DSA/Java/ICPC/src/Problems/sheet_1/A.java
@@ -0,0 +1,11 @@
+package Problems.sheet_1;
+
+import java.util.Scanner;
+public class A {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        System.out.println("Hello, " + S);
+    }
+}
