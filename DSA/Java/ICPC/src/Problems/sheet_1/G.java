25be79d9c52ae205259c85f259cac2a155edf792
Saransh Nimje <saranshnimje0@gmail.com>
2026-09-09 20:09:27 +0530

    Create G.java

---
+++ DSA/Java/ICPC/src/Problems/sheet_1/G.java
@@ -0,0 +1,13 @@
+package Problems.sheet_1;
+
+import java.util.Scanner;
+
+public class G {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long sum = N*(N+1)/2;
+        System.out.println(sum);
+    }
+}
