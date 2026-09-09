a31a5ed2d5a2717618be6ee0c676432bb5151a6c
Saransh Nimje <saranshnimje0@gmail.com>
2026-09-09 19:53:30 +0530

    Create E.java

---
+++ DSA/Java/ICPC/src/Problems/sheet_1/E.java
@@ -0,0 +1,13 @@
+package Problems.sheet_1;
+
+import java.util.Scanner;
+public class E {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double R = sc.nextDouble();
+        double pi = 3.141592653;
+        double Area = pi * R * R;
+        System.out.println(Area);
+    }
+}
